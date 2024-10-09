e731dc4e91486c6a5e4144a7b783f76742fb120b
MuhammadAgungPutraNugroho <muhammadagung0708@example.com>
2024-10-09 21:50:17 +0700

    perbaikan product overview

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -13,7 +13,7 @@
             @foreach ($dataProducts as $data)
                 @if ($loop->iteration % 2 == 0)
                     <!-- Jika genap, gambar di kanan -->
-                    <div class="row gx-5 mb-3">
+                    <div class="row gx-5 mb-3" data-aos="fade-right">
                         <div class="col-lg-7">
                             <div class="mb-4">
                                 <h1 class="display-4 text-kitaanter">{{ $data->title }}</h1>
@@ -29,7 +29,7 @@
                     </div>
                 @else
                     <!-- Jika ganjil, gambar di kiri -->
-                    <div class="row gx-5 mb-3">
+                    <div class="row gx-5 mb-3" data-aos="fade-left">
                         <div class="col-lg-5 mb-5 mb-lg-0" style="min-height: 500px;">
                             <div class="position-relative h-100">
                                 <img class="position-absolute w-100 h-100 rounded"
